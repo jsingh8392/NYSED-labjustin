be0c66abe885a87fcc6cdd11fcd1e1e05f514b25
jsingh8392 <jsingh8392@richmondhillhs.org>
2022-01-12 16:53:26 +0000

    finishedNYS

--- src/components/SchoolCard.js
+++ src/components/SchoolCard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './css/SchoolCard.css';
 
-function SchoolCard() {
+function SchoolCard(props) {
   return(
     <div className="col-4">
       <div className="card">
-        <h5 className="card-header">PASS SCHOOL NAME HERE</h5>
+        <h5 className="card-header">{props.school.ENTITY_NAME}</h5>
         <div className="card-body">
           <h5 className="card-title">
             <span className="badge">2017-18</span>
@@ -14,14 +14,14 @@ function SchoolCard() {
           </h5>
           <span className="badge badge-header">Student Data</span>
           <ul className="list-group">
-            <li className="list-group-item">Enrollment Data</li>
-            <li className="list-group-item">3-8 ELA Assessment Data</li>
-            <li className="list-group-item">3-8 Math Assessment Data</li>
+            <li className="list-group-item">Number of White{props.school.NUM_WHITE}</li>
+            <li className="list-group-item">Number of Hispanic{props.school.NUM_HISP}</li>
+            <li className="list-group-item">Number of Asian{props.school.NUM_ASIAN}</li>
           </ul>
           <span className="badge badge-header">School Data</span>
           <ul className="list-group">
             <li className="list-group-item">School Report Card</li>
-            <li className="list-group-item">Student and Educator Report</li>
+            <li className="list-group-item"></li>
           </ul>
         </div>
       </div>
